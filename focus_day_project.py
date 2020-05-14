de7df2a39509fc39c77daeab135165ed878e6bc0
natanbrook <23nbrook@athenian.org>
2020-05-14 18:44:45 +0000

    still working

--- focus_day_project.py
+++ focus_day_project.py
@@ -1,8 +1,76 @@
-## Hello Middle Eighth Grader, This project is to test your knowledge and to see your ability to make smart and safe decisions if you were ever to be stuck on a stranded island!
+## Hello Eighth Grader, This project is to test your knowledge and to see your ability to make smart and safe decisions if you were ever to be stuck on a stranded island!
 #You will be asked several two option questions, one answer is correct and if you choose it you will move on to the next question, if you choose the incorrect answer you will have to start over again.
 
 # One day you decided to hop on a plane and go on a short vacation. While you were in the air the plane started having a bunch of turbulance. You thought it was nothing so you decided to take a nap. The moment you wake up everyonen around you is screaming and you can tell the plane is going down. You start freaking out. You are so scared that you completely black out. 
 # The next thing you know, your beached on an island. Youre alone, scared, confused, and the only thing you have is the pocket knife you keep in your back pocket at all times. 
 # What do you do now?
 
-Input: "What do you do first?" 
+questionone = input("What is the #1 thing that you should never lose?- a.)Your pocket knife. b.) Your clothes")
+questionones = ("a")
+while questionone == questionones:
+    print("you got it right!")
+else:
+    print("you got it wrong!")
+
+questiontwo = input("What is the first thing you should do/look for?- a.)water. b.) food")
+questiontwos = ("a")
+while questiontwo == questiontwos:
+    print("you got it right!")
+else:
+    print("you got it wrong!")
+
+questionthree = input("What is the #1 thing that you should never lose?- a.)Your pocket knife. b.) Your clothes")
+questionthrees = ("a")
+while questionthree == questionthrees:
+    print("you got it right!")
+else:
+    print("you got it wrong!")
+
+questionfour = input("What is the #1 thing that you should never lose?- a.)Your pocket knife. b.) Your clothes")
+questionfours = ("a")
+while questionfour == questionfours:
+    print("you got it right!")
+else:
+    print("you got it wrong!")
+
+questionfive = input("What is the #1 thing that you should never lose?- a.)Your pocket knife. b.) Your clothes")
+questionfives = ("a")
+while questionfive == questionfives:
+    print("you got it right!")
+else:
+    print("you got it wrong!")
+
+questionsix = input("What is the #1 thing that you should never lose?- a.)Your pocket knife. b.) Your clothes")
+questionsixs = ("a")
+while questionsix == questionsixs:
+    print("you got it right!")
+else:
+    print("you got it wrong!")
+
+questionone = input("What is the #1 thing that you should never lose?- a.)Your pocket knife. b.) Your clothes")
+questionones = ("a")
+while questionone == questionones:
+    print("you got it right!")
+else:
+    print("you got it wrong!")
+
+questionone = input("What is the #1 thing that you should never lose?- a.)Your pocket knife. b.) Your clothes")
+questionones = ("a")
+while questionone == questionones:
+    print("you got it right!")
+else:
+    print("you got it wrong!")
+
+questionone = input("What is the #1 thing that you should never lose?- a.)Your pocket knife. b.) Your clothes")
+questionones = ("a")
+while questionone == questionones:
+    print("you got it right!")
+else:
+    print("you got it wrong!")
+
+questionone = input("What is the #1 thing that you should never lose?- a.)Your pocket knife. b.) Your clothes")
+questionones = ("a")
+while questionone == questionones:
+    print("you got it right!")
+else:
+    print("you got it wrong!")
