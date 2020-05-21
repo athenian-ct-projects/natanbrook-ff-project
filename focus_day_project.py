88c1acab05a1ef563717f8693079c19fe1295f40
natanbrook <23nbrook@athenian.org>
2020-05-21 18:31:24 +0000

    finished

--- focus_day_project.py
+++ focus_day_project.py
@@ -1,5 +1,5 @@
 ## Hello Eighth Grader, This project is to test your knowledge and to see your ability to make smart and safe decisions if you were ever to be stuck on a stranded island!
-#You will be asked several two option questions, one answer is correct and if you choose it you will move on to the next question, if you choose the incorrect answer you will have to start over again.
+#You will be asked several two option questions, one answer is correct and if you choose it you will move on to the next question, if you choose the incorrect answer you will get to move on, but you wont have a perfect score
 
 # One day you decided to hop on a plane and go on a short vacation. While you were in the air the plane started having a bunch of turbulance. You thought it was nothing so you decided to take a nap. The moment you wake up everyonen around you is screaming and you can tell the plane is going down. You start freaking out. You are so scared that you completely black out. 
 # The next thing you know, your beached on an island. Youre alone, scared, confused, and the only thing you have is the pocket knife you keep in your back pocket at all times. 
@@ -33,43 +33,43 @@ while questionfour == questionfours:
 else:
     print("you got it wrong! Never eat unidentified berries!")
 
-questionfive = input("What is the #1 thing that you should never lose?- a.)Your pocket knife. b.) Your clothes")
-questionfives = ("a")
+questionfive = input("What is the most dangerous sea life animal you could encounter?- a.) sharks and eels. b.) Sea Wasp Jellyfish ")
+questionfives = ("b")
 while questionfive == questionfives:
     print("you got it right!")
 else:
-    print("you got it wrong!")
+    print("you got it wrong! Sea Wasp Jellyfish are the most dangerous creatures in the ocean")
 
-questionsix = input("What is the #1 thing that you should never lose?- a.)Your pocket knife. b.) Your clothes")
+questionsix = input("What is the most deadly land animal you could encounter?- a.)Snakes. b.) Spiders")
 questionsixs = ("a")
 while questionsix == questionsixs:
     print("you got it right!")
 else:
     print("you got it wrong!")
 
-questionseven = input("What is the #1 thing that you should never lose?- a.)Your pocket knife. b.) Your clothes")
+questionseven = input(" What is one insect that you need to always be looking out for?- a.)Mosquitos. b.) Stink bugs")
 questionsevens = ("a")
 while questionseven == questionsevens:
     print("you got it right!")
 else:
-    print("you got it wrong!")
+    print("you got it wrong! Mosquitos have the highest kill rate than any other insect!")
 
-questioneight = input("What is the #1 thing that you should never lose?- a.)Your pocket knife. b.) Your clothes")
-questioneights = ("a")
+questioneight = input("How long can you survive without food?- a.)5 days. b.) three weeks")
+questioneights = ("b")
 while questioneight == questioneights:
     print("you got it right!")
 else:
     print("you got it wrong!")
 
-questionnine = input("What is the #1 thing that you should never lose?- a.)Your pocket knife. b.) Your clothes")
-questionnines = ("a")
+questionnine = input("What plants can you use for a soap replacement?- a.)Sunflowers. b.) Yucca Roots")
+questionnines = ("b")
 while questionnine == questionnines:
     print("you got it right!")
 else:
     print("you got it wrong!")
 
-questionten = input("What is the #1 thing that you should never lose?- a.)Your pocket knife. b.) Your clothes")
-questiontens = ("a")
+questionten = input("How long can you survive without water?- a.) 6 days. b.) 3 days")
+questiontens = ("b")
 while questionten == questiontens:
     print("you got it right!")
 else:
