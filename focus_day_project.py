22526cf39b376f21d6fa58e56bf0ad8a90be80b1
natanbrook <23nbrook@athenian.org>
2020-05-29 16:51:36 +0000

    added comments and conditional but cant figure out function

--- focus_day_project.py
+++ focus_day_project.py
@@ -7,81 +7,73 @@
 
 questionone = input("What is the #1 thing that you should never lose?- a.)Your clothes. b.) Your pocket knife")
 questionones = ("a")
-while questionone == questionones:
-  print("you got it right!")
-  break 
+if questionone == questionones:
+  print("you got it right!") 
 else:
     print("you got it wrong!")
+# I set question 1 as the question and i put question1s as the answer to the question. 
+# I then have my if statement to print the right answer and else statement to print the incorrect answer
 #all of the questions will be set up like this, two options and one answer.
 questiontwo = input("What is the first thing you should look for?- a.)water. b.) food")
 questiontwos = ("a")
-while questiontwo == questiontwos:
+if questiontwo == questiontwos:
     print("you got it right!")
-    break
 else:
     print("you got it wrong!")
 #a lot of questions will just be testing knowledge on facts 
 questionthree = input("Where should you make your camp?- a.)near many trees. b.) near the shore")
 questionthrees = ("b")
-while questionthree == questionthrees:
+if questionthree == questionthrees:
     print("you got it right!")
-    break
 else:
     print("you got it wrong! You need to stay on the shore in case someone is flying over and can see you!")
 
 questionfour = input("You see random berries sitting on a bush, what do you do?- a.) Eat them! b.) Leave them be")
 questionfours = ("b")
-while questionfour == questionfours:
+if questionfour == questionfours:
     print("you got it right!")
-    break
 else:
     print("you got it wrong! Never eat unidentified berries!")
 
 questionfive = input("What is the most dangerous sea life animal you could encounter?- a.) sharks and eels. b.) Sea Wasp Jellyfish ")
 questionfives = ("b")
-while questionfive == questionfives:
+if questionfive == questionfives:
     print("you got it right!")
-    break
 else:
     print("you got it wrong! Sea Wasp Jellyfish are the most dangerous creatures in the ocean")
 
 questionsix = input("What is the most deadly land animal you could encounter?- a.)Snakes. b.) Spiders")
 questionsixs = ("a")
-while questionsix == questionsixs:
+if questionsix == questionsixs:
     print("you got it right!")
-    break
 else:
     print("you got it wrong!")
 
 questionseven = input(" What is one insect that you need to always be looking out for?- a.)Mosquitos. b.) Stink bugs")
 questionsevens = ("a")
-while questionseven == questionsevens:
+if questionseven == questionsevens:
     print("you got it right!")
-    break
 else:
     print("you got it wrong! Mosquitos have the highest kill rate than any other insect!")
 
 questioneight = input("How long can you survive without food?- a.)5 days. b.) three weeks")
 questioneights = ("b")
-while questioneight == questioneights:
+if questioneight == questioneights:
     print("you got it right!")
-    break
 else:
     print("you got it wrong!")
 
 questionnine = input("What plants can you use for a soap replacement?- a.)Sunflowers. b.) Yucca Roots")
 questionnines = ("b")
-while questionnine == questionnines:
+if questionnine == questionnines:
     print("you got it right!")
-    break
 else:
     print("you got it wrong!")
 
 questionten = input("How long can you survive without water?- a.) 6 days. b.) 3 days")
 questiontens = ("b")
-while questionten == questiontens:
+if questionten == questiontens:
     print("you got it right!")
-    break
 else:
     print("you got it wrong!")
 
